fdfa33e4c652843c58474a17b8a99e812def11de
giyhun <ir47839@gmail.com>
2014-11-29 09:28:17 +0900

    Update select.rb

--- group-tables/select.rb
+++ group-tables/select.rb
@@ -3,12 +3,13 @@ class GroupTable < Controller
   def start
     p "start group"
   end
+  
   def packet_in dpid, mes
     p "packet in from #{dpid} with port #{mes.in_port}"
   end
+  
   def switch_ready dpid
     p "wake up #{dpid}"
-
     action = SendOutPort.new(port_number:2)
     bucket1 = Bucket.new(
       weight:10,
@@ -30,7 +31,6 @@ class GroupTable < Controller
       }
     )
 
-   
     action = GroupAction.new(2)
     ins = ApplyAction.new( actions:[action] )
     send_flow_mod(
